ee845656b5536d556290c44405e475c90f959308
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2022-11-09 20:23:09 +0530

    Added my day-2 Tasks

--- pharmasy/src/App.js
+++ pharmasy/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Home} from "./LokeshPages/Home";
 
 
 
 function App() {
   return (
     <div className="App" >
-     
+     <Home/>
     </div>
   );
 }
