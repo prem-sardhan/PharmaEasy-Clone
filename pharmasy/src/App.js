960a96aef55cdbdf0a58c4c9ae80b83cf5f5826a
prem-sardhan <premsardhan@gmail.com>
2022-11-09 12:20:13 +0530

    first commit

--- pharmasy/src/App.js
+++ pharmasy/src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
+     
      
      
     </div>
--- pharmasy/src/index.js
+++ pharmasy/src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './prem/Components/Context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <BrowserRouter>
+      <ChakraProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ChakraProvider>
+    </BrowserRouter>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
