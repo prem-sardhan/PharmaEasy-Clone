92ce2d8c5700f5b014d1a2080fee6c98e86ab143
abik00 <abikp00@gmail.com>
2022-11-09 20:02:26 +0530

    abhay

--- pharmasy/src/App.js
+++ pharmasy/src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import CardSlider from './Rohit_components/CardSlide'
 
+import Category from './Abhay_components/Category';
+import { Box } from '@chakra-ui/react';
 
 
 
 function App() {
   return (
     <div className="App" >
-     
+    
+      <h1>hello world</h1>
+      <Box display='flex' justifyContent='space-evenly' border='1px solid black' >
+          <Category/>
+      </Box>
     </div>
   );
 }
